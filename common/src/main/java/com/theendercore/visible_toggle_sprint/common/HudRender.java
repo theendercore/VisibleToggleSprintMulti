eeaa7e78e8b964ded48ef5b08fcc3c55aecf911d
TheEnderCore <theendercore@gmail.com>
2025-04-28 15:47:37 +0300

    - hotbar icons have been moved to the atlas - fixed crosshair icon not scaling well

--- common/src/main/java/com/theendercore/visible_toggle_sprint/common/HudRender.java
+++ common/src/main/java/com/theendercore/visible_toggle_sprint/common/HudRender.java
@@ -22,8 +22,8 @@ public class HudRender {
         if (client.player == null) return;
         Options options = client.options;
 
-        int sWidth = gui.guiWidth() / 2;
-        int sHeight = gui.guiHeight();
+        int width = gui.guiWidth();
+        int height = gui.guiHeight();
 
         boolean debug = !client.gui.getDebugOverlay().showDebugScreen();
 
@@ -33,10 +33,10 @@ public class HudRender {
         if (client.gameMode != null && client.gameMode.getPlayerMode() == GameType.SPECTATOR) return;
 
         if (shouldRender(CONFIG.sprint, options.keySprint.isDown(), client.player.isSprinting())) {
-            renderIndicator(CONFIG.sprint, debug, client, options, gui, sWidth, sHeight, "sprint");
+            renderIndicator(CONFIG.sprint, debug, client, options, gui, width, height, "sprint");
         }
         if (shouldRender(CONFIG.sneak, options.keyShift.isDown(), client.player.isCrouching())) {
-            renderIndicator(CONFIG.sneak, debug, client, options, gui, sWidth, sHeight, "sneak");
+            renderIndicator(CONFIG.sneak, debug, client, options, gui, width, height, "sneak");
         }
     }
 
@@ -48,18 +48,26 @@ public class HudRender {
         };
     }
 
-    public static void renderIndicator(VisibleToggleSprintConfig.PlayerState state, boolean debug, Minecraft client, Options options, GuiGraphics gui, int sWidth, int sHeight, String langKey) {
+    public static void renderIndicator(VisibleToggleSprintConfig.PlayerState state, boolean debug, Minecraft client, Options options, GuiGraphics gui, int width, int height, String type) {
         assert client.player != null;
         if ((debug || client.player.isReducedDebugInfo()) && options.getCameraType().isFirstPerson() && state.crosshairEnable) {
             RenderSystem.enableBlend();
             RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.ONE_MINUS_DST_COLOR, GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-            gui.blit(MOD_ICONS, sWidth + state.crosshairX, (sHeight / 2) + state.crosshairY, state.crosshairIcon.get().x, 0, 4, 4);
+            gui.blit(MOD_ICONS,
+                    (width - 4 + state.crosshairX) / 2, (height - 4 + state.crosshairY) / 2,
+                    state.crosshairIcon.get().x,
+                    0, 4, 4);
             RenderSystem.defaultBlendFunc();
             RenderSystem.disableBlend();
         }
-        if (state.hotbarEnable)
-            gui.blit(MOD_ICONS, sWidth + state.hotbarX, (sHeight - state.hotbarY), 0, 16, 16, 16);
+        if (state.hotbarEnable) {
+            gui.blitSprite(id("hud/" + type),
+                    16, 16,
+                    0, 0,
+                    (width / 2) + state.hotbarX, (height - state.hotbarY),
+                    16, 16);
+        }
         if (debug && state.textEnable)
-            gui.drawString(client.font, Component.translatable("hud.visible_toggle_sprint." + langKey), state.textX, state.textY, state.textColor.toInt(), true);
+            gui.drawString(client.font, Component.translatable("hud.visible_toggle_sprint." + type), state.textX, state.textY, state.textColor.toInt(), true);
     }
 }
